1fd873b22555d19a8cea91c273e0633edee82ee7
Hgyeol <neunimchi626@gmail.com>
2024-11-03 20:32:54 +0900

    DuckVirus1.1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ setInterval(()=> {
         a.src = "https://img.notionusercontent.com/s3/prod-files-secure%2F20d51118-9446-45f7-988b-31afd0991192%2Fceaa1943-03e1-4669-95c2-bfe72dbb3302%2FKakaoTalk_Photo_2024-09-23-15-56-34.png/size/w=1280?exp=1730632951&sig=8A5RoiiHTqRZxR5O8Nc0CTCRnehp-J08baAkNUNhKJc"
     })
 
-    let textElements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span, div'); 
+    let textElements = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span'); 
     textElements.forEach((el) => {
         el.innerText = "공덕공덕공덕현"; 
     })
